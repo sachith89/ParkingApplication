bde27172ff808467de28c933ddb496c030a8967c
sachith <sachith@preseria.app>
2025-07-22 19:02:47 +0530

    Add project documentation and refine Maven configuration

--- src/main/java/dev/sachith/parkingapplication/ParkingApplication.java
+++ src/main/java/dev/sachith/parkingapplication/ParkingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ParkingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParkingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingApplication.class, args);
+    }
 
 }
